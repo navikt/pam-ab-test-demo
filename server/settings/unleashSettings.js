092141b8e080c71671aab1c414fd729ab97508b1
Julian Torp <julian.torp@nav.no>
2020-09-23 01:22:32 +0200

    add logs to unleash

--- server/settings/unleashSettings.js
+++ server/settings/unleashSettings.js
@@ -1,8 +1,12 @@
 const { initialize } = require('unleash-client');
 
-const setupUnleash = () => initialize({
+const unleash = initialize({
   url: 'https://unleashproxy.nais.oera.no/api/',
   appName: 'pam-ab-test-demo',
 });
 
-module.exports = setupUnleash();
+unleash.on('error', console.error);
+unleash.on('warning', console.warn);
+unleash.on('ready', console.info);
+
+module.exports = unleash;
