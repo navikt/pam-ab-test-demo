85ed6155b636427f56a30a29ac442a36ae6448b4
Julian Torp <julian.torp@nav.no>
2020-09-23 21:48:17 +0200

    fix static serving

--- server/settings/setupDistributionRoutes.js
+++ server/settings/setupDistributionRoutes.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
+const { distributionPaths } = require('./configureDistributions');
 const { userTestGroup } = require('./setupTestGroupInterceptor');
 
 const setupDistributionRoutes = (server) => {
-  server.use('/', express.static(path.resolve(__dirname, '../../dist')));
+  const paths = distributionPaths().map((it) => it.split('/')[1]);
+  server.use(paths, (p) => express.static(path.resolve(__dirname, `../../dist/${p}/img`)));
   server.use(['/'], (req, res) => {
     const testGroup = userTestGroup(req);
-    if (testGroup) {
+    if (testGroup && paths.includes(testGroup)) {
       return res.sendFile(path.resolve(__dirname, `../../dist/${testGroup}`, 'index.html'));
     }
     return res.sendFile(path.resolve(__dirname, '../../dist/master', 'index.html'));
